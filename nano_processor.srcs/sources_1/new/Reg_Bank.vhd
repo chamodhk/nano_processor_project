716b1a66bea21be8e32942ed7c8ac15be62a7849
Daksitha Wijerathne <192776007+ddwijerathne@users.noreply.github.com>
2025-05-14 10:23:28 +0530

    reg bank intialized

---
+++ nano_processor.srcs/sources_1/new/Reg_Bank.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/14/2025 10:18:19 AM
+-- Design Name: 
+-- Module Name: Reg_Bank - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Reg_Bank is
+    Port ( Clk : in STD_LOGIC;
+           Reg_sel : in STD_LOGIC_VECTOR (2 downto 0);
+           Input_val : in STD_LOGIC_VECTOR (3 downto 0);
+           Reset : in STD_LOGIC;
+           Q0 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q1 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q2 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q3 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q4 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q5 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q6 : out STD_LOGIC_VECTOR (3 downto 0);
+           Q7 : out STD_LOGIC_VECTOR (3 downto 0));
+end Reg_Bank;
+
+architecture Behavioral of Reg_Bank is
+
+begin
+
+
+end Behavioral;
