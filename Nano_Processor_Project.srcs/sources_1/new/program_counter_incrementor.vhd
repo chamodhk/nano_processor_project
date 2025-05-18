9a6431f1981c1f12b04245b35fd4a5a7fbd1e034
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-05-18 14:41:42 +0530

    added the increment enable port to the program counter incrementor

--- Nano_Processor_Project.srcs/sources_1/new/program_counter_incrementor.vhd
+++ Nano_Processor_Project.srcs/sources_1/new/program_counter_incrementor.vhd
@@ -1,23 +1,26 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;  -- Required for arithmetic
+use IEEE.NUMERIC_STD.ALL;
 use work.buses.instruction_address;
 
 entity program_counter_incrementor is   
     Port (
-        in_address  : in  instruction_address;
-        out_address : out instruction_address
+        in_address       : in  instruction_address;
+        increment_enable : in  std_logic;
+        out_address      : out instruction_address
     );
 end program_counter_incrementor;
 
 architecture Behavioral of program_counter_incrementor is
 begin
-
-    process(in_address)
+    process(in_address, increment_enable)
         variable temp : unsigned(2 downto 0);
     begin
-        temp := unsigned(in_address) + 1;
+        if increment_enable = '1' then
+            temp := unsigned(in_address) + 1;
+        else
+            temp := unsigned(in_address);  -- Hold the current address
+        end if;
         out_address <= std_logic_vector(temp);
     end process;
-
 end Behavioral;
